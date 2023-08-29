f3d6b1d2ded11bac73903709edab691e2e9b4a49
Andy Godoy <Andygodoy@ymail.com>
2023-08-29 16:10:40 -0700

    got my Jate working

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -13,28 +11,38 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+
+    // setting output
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
+    // plugins
     plugins: [
+
+      // making html
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'J.A.T.E'
       }),
-      new MiniCssExtractPlugin(),
+
+      // service workers
       new InjectManifest({
-        swSrc: './service-worker.js',
-        swDest: 'service-worker.js',
-      }), 
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+
+      // Installation
       new WebpackPwaManifest({
         name: 'JATE',
         short_name: 'JATE',
-        description: 'Do stuff!',
+        fingerprints: false,
+        description: 'Keep track of important tasks!',
         background_color: '#7eb4e2',
         theme_color: '#7eb4e2',
-        start_url: '/',
-        publicPath: '/',
+        start_url: './',
+        publicPath: './',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
@@ -51,6 +59,8 @@ module.exports = () => {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
+
+        // for converting ES6 and above to ES5
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
